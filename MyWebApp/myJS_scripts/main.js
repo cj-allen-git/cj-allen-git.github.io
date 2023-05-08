bec9fbd0b766e180b6c96a4e0fcadfd5f1078f57
cj-allen-git <ccallen93@gmail.com>
2023-05-08 09:40:16 -0600

    updated web app folder for project presentation

--- MyWebApp/myJS_scripts/main.js
+++ MyWebApp/myJS_scripts/main.js
@@ -1,9 +1,10 @@
 Promise.all([d3.csv("data/nmjustice40.csv"), d3.csv("data/foodaccessnm.csv"), d3.json("data/NewMexico10.json")])
-    .then(function([justic_data, food_data, nm_json]) {
+    .then(function ([justic_data, food_data, nm_json]) {
         draw(justic_data, food_data, nm_json);
         //  returnObject = drawBubbles(demo2, demo2_geojson);
     });
 
+// var exceed = Number(d.Excede)
 
 function draw(justic_data, food_data, nm_json) {
     // dataP = [];
@@ -34,20 +35,50 @@ function draw(justic_data, food_data, nm_json) {
     // ndx = crossfilter(data_merged);
 
     ndx = crossfilter(food_data);
-    var allDim = ndx.dimension(function(d) {
+    var allDim = ndx.dimension(function (d) {
         return d;
     });
 
-    var countyDim = ndx.dimension(function(d) {
+    var countyDim = ndx.dimension(function (d) {
         return d["County"];
     });
-    var lowIncomeDim = ndx.dimension(function(d) {
-        return d["LowIncomeTracts"];
+
+    var foodDim = ndx.dimension(function (d) {
+        return d["LATracts1"];
+    })
+
+    var food10Dim = ndx.dimension(function (d) {
+        return d["LATracts10"]
+    })
+
+    var food20Dim = ndx.dimension(function (d) {
+        return d["LATracts20"]
+    })
+
+    var foodVehDim = ndx.dimension(function (d) {
+        return d["HUNVFlag"]
+    })
+
+    var catsExceededDim = ndx.dimension(function (d) {
+        return d["Excede"]
+    })
+
+    var lowIncomeDim = ndx.dimension(function (d) {
+        return d["PovertyRate"];
     });
+
+    var percBlackDim = ndx.dimension(function (d) {
+        return d["lablack1share"];
+    })
+
+    var percHispDim = ndx.dimension(function (d) {
+        return d["lahisp1share"];
+    })
+
     // var pop2010Dim = ndx.dimension(function(d) {
     //     return d["Pop2010"];
     // });
-    var mapDim = ndx.dimension(function(d) {
+    var mapDim = ndx.dimension(function (d) {
         return d["GEOID10"];
     });
 
@@ -56,16 +87,30 @@ function draw(justic_data, food_data, nm_json) {
     // var pop2010Group = pop2010Dim.group().reduceCount();
 
     var countyGroup = countyDim.group().reduceCount();
+    var foodGroup = foodDim.group().reduceCount();
+    var food10Group = food10Dim.group().reduceCount();
+    var food20Group = food20Dim.group().reduceCount();
+    var foodVehGroup = foodVehDim.group().reduceCount();
+    var catsExceededGroup = catsExceededDim.group().reduceCount();
     var lowIncomeGroup = lowIncomeDim.group().reduceCount();
-    var mapGroup = mapDim.group().reduceSum(function(d) {
-        return d.Pop2010;
+    var percBlackGroup = percBlackDim.group().reduceCount();
+    var percHispGroup = percHispDim.group().reduceCount();
+    var mapGroup = mapDim.group().reduceSum(function (d) {
+        return d.Excede;
     });
 
 
     var countyChart = dc.pieChart('#chart-ring-county', groupname);
+    var foodChart = dc.pieChart('#chart-ring-food', groupname);
+    var food10Chart = dc.pieChart('#chart-ring-food10', groupname);
+    var food20Chart = dc.pieChart('#chart-ring-food20', groupname);
+    var foodVehChart = dc.pieChart('#chart-ring-foodVeh', groupname);
+    var catsExceededChart = dc.pieChart('#chart-ring-cats', groupname)
     var lowIncomeChart = dc.barChart('#chart-ring-lowIncome', groupname);
+    var percBlackChart = dc.barChart('#chart-ring-Black', groupname);
+    var percHispChart = dc.barChart('#chart-ring-Hisp', groupname);
     var map = dc_leaflet.choroplethChart("#map", groupname)
-        // var map = dc_leaflet.choroplethChart("#map", groupname)
+    // var map = dc_leaflet.choroplethChart("#map", groupname)
 
     var dataTableCount = dc.dataCount('.dc-dataTable-count', groupname);
     var dataTable = dc_datatables.datatable('#data-table', groupname);
@@ -77,7 +122,7 @@ function draw(justic_data, food_data, nm_json) {
     map
         .dimension(mapDim)
         .group(mapGroup)
-        .width(400)
+        .width(600)
         .height(400)
         .center([34.528072, -106.007275])
         .zoom(6)
@@ -87,18 +132,28 @@ function draw(justic_data, food_data, nm_json) {
             d3.min(mapGroup.all(), dc.pluck('value')),
             d3.max(mapGroup.all(), dc.pluck('value'))
         ])
-        .colorAccessor(function(d, i) {
+        .colorAccessor(function (d, i) {
             return d.value;
         })
-        .featureKeyAccessor(function(feature) {
+        .featureKeyAccessor(function (feature) {
             return feature.properties.GEOID10;
         })
         .popupMod('ctrlCmd')
         .renderPopup(true)
-        .popup(function(d, feature) {
-            return feature.properties.TRACTCE10 + " : " + d.Pop2010;
+        .popup(function (d, feature) {
+            return "2010 Population: " + d.feature["Pop2010"] <br> "Tract SNAP: " + d.TractSNAP;
         })
-        .legend(dc_leaflet.legend().position('bottomright'));
+        .legend(dc_leaflet.legend().position('bottomright').legendTitle("Burdens exceeded:"));
+
+
+    /*  L.geoJSON(foodaccessnm, {
+          onEachFeature: function (feature, layer) {
+            if (feature.properties && feature.properties.GEIOD10) {
+              layer.bindPopup('<h3><p>2010 Population ' + feature.properties["POP2010"] + '</h3></p>');
+            }
+          },
+      }).addTo(mapObject);
+*/
 
     countyChart
         .width(245)
@@ -111,21 +166,97 @@ function draw(justic_data, food_data, nm_json) {
             .horizontal(false)
             .highlightSelected(true));
 
-    lowIncomeChart
+
+    foodChart
         .width(245)
+        .height(280)
+        .innerRadius(70)
+        .dimension(foodDim)
+        .group(foodGroup)
+        .colors(d3.scaleOrdinal().domain([0, 1]))
+        .range(d3.schemeRdBu)
+        //.ordinalColors(d3.schemeSet1)
+        .legend(new dc.HtmlLegend()
+            .container('#food-legend')
+            .horizontal(false)
+            .highlightSelected(true));
+
+    food10Chart
+        .width(245)
+        .height(280)
+        .innerRadius(70)
+        .dimension(food10Dim)
+        .group(food10Group)
+        .legend(new dc.HtmlLegend()
+            .container('#food10-legend')
+            .horizontal(false)
+            .highlightSelected(true));
+
+    food20Chart
+        .width(245)
+        .height(280)
+        .innerRadius(70)
+        .dimension(food20Dim)
+        .group(food20Group)
+        .legend(new dc.HtmlLegend()
+            .container('#food20-legend')
+            .horizontal(false)
+            .highlightSelected(true));
+
+    foodVehChart
+        .width(245)
+        .height(280)
+        .innerRadius(70)
+        .dimension(foodVehDim)
+        .group(foodVehGroup)
+        .legend(new dc.HtmlLegend()
+            .container('#foodVeh-legend')
+            .horizontal(false)
+            .highlightSelected(true));
+
+    catsExceededChart
+        .width(245)
+        .height(280)
+        .innerRadius(70)
+        .dimension(catsExceededDim)
+        .group(catsExceededGroup)
+        .colors(d3.scaleOrdinal().domain(['0', '1', '2', '3', '4', '5', '6', '7']))
+        .range(['#F6ED6D', '#DAD176', '#BFB57F', '#BFB57F', '#887E91', '#6C629A', '#5146A3', '#362BAC'])
+        .legend(new dc.HtmlLegend()
+            .container('#cats-legend')
+            .horizontal(false)
+            .highlightSelected(true));
+
+
+    percBlackChart
+        .width(300)
+        .height(250)
+        .x(d3.scaleBand())
+        .xUnits(dc.units.ordinal)
+        .brushOn(false)
+        .yAxisLabel("Percent Black")
+        .dimension(percBlackDim)
+        .group(percBlackGroup)
+
+    percHispChart
+        .width(300)
+        .height(250)
+        .x(d3.scaleBand())
+        .xUnits(dc.units.ordinal)
+        .brushOn(false)
+        .yAxisLabel("Percent Hispanic")
+        .dimension(percHispDim)
+        .group(percHispGroup)
+
+    lowIncomeChart
+        .width(1200)
         .height(280)
         .x(d3.scaleBand())
         .xUnits(dc.units.ordinal)
         .brushOn(false)
-        .yAxisLabel("This is the Y Axis!")
+        .yAxisLabel("Poverty rate")
         .dimension(lowIncomeDim)
         .group(lowIncomeGroup)
-        // .on('renderlet', function(chart) {
-        //     chart.selectAll('rect').on("click", function(d) {
-        //         console.log("click!", d);
-        //     });
-        // });
-
 
     dataCount
         .dimension(ndx)
@@ -137,114 +268,114 @@ function draw(justic_data, food_data, nm_json) {
 
     dataTable
         .dimension(allDim)
-        .group(function(d) {
+        .group(function (d) {
             return 'dc.js insists on putting a row here so I remove it using JS';
         })
         .size(10)
         .columns([{
-                label: 'GEOID10',
-                type: 'string',
-                format: function(d) {
-                    return d["GEOID10"];
-                }
-
-            }, {
-                label: 'Pop2010',
-                type: 'num',
-                format: function(d) {
-                    return d["Pop2010"];
-                }
-            }, {
-                label: 'State',
-                type: 'num',
-                format: function(d) {
-                    return d["State"];
-                }
-            }, {
-                label: 'County',
-                type: 'num',
-                format: function(d) {
-                    return d["County"];
-                }
-            }, {
-                label: 'OHU2010',
-                type: 'num',
-                format: function(d) {
-                    return d["OHU2010"];
-                }
-            }, {
-                label: 'GroupQuartersFlag',
-                type: 'num',
-                format: function(d) {
-                    return d["GroupQuartersFlag"];
-                }
-            }, {
-                label: 'NUMGQTRS',
-                type: 'num',
-                format: function(d) {
-                    return d["NUMGQTRS"];
-                }
-            }, {
-                label: 'LILATracts_1And10',
-                type: 'num',
-                format: function(d) {
-                    return d["LILATracts_1And10"];
-                }
-            }, {
-                label: 'MedianFamilyIncome',
-                type: 'num',
-                format: function(d) {
-                    return d["MedianFamilyIncome"];
-                }
-            }, {
-                label: 'PCTGQTRS',
-                type: 'num',
-                format: function(d) {
-                    return d["PCTGQTRS"];
-                }
-            }, {
-                label: 'PovertyRate',
-                type: 'num',
-                format: function(d) {
-                    return d["PovertyRate"];
-                }
-            }, {
-                label: 'LowIncomeTracts',
-                type: 'num',
-                format: function(d) {
-                    return d["LowIncomeTracts"];
-                }
+            label: 'GEOID10',
+            type: 'string',
+            format: function (d) {
+                return d["GEOID10"];
             }
 
+        }, {
+            label: 'Pop2010',
+            type: 'num',
+            format: function (d) {
+                return d["Pop2010"];
+            }
+        }, {
+            label: 'State',
+            type: 'num',
+            format: function (d) {
+                return d["State"];
+            }
+        }, {
+            label: 'County',
+            type: 'num',
+            format: function (d) {
+                return d["County"];
+            }
+        }, {
+            label: 'OHU2010',
+            type: 'num',
+            format: function (d) {
+                return d["OHU2010"];
+            }
+        }, {
+            label: 'GroupQuartersFlag',
+            type: 'num',
+            format: function (d) {
+                return d["GroupQuartersFlag"];
+            }
+        }, {
+            label: 'NUMGQTRS',
+            type: 'num',
+            format: function (d) {
+                return d["NUMGQTRS"];
+            }
+        }, {
+            label: 'LILATracts_1And10',
+            type: 'num',
+            format: function (d) {
+                return d["LILATracts_1And10"];
+            }
+        }, {
+            label: 'MedianFamilyIncome',
+            type: 'num',
+            format: function (d) {
+                return d["MedianFamilyIncome"];
+            }
+        }, {
+            label: 'PCTGQTRS',
+            type: 'num',
+            format: function (d) {
+                return d["PCTGQTRS"];
+            }
+        }, {
+            label: 'PovertyRate',
+            type: 'num',
+            format: function (d) {
+                return d["PovertyRate"];
+            }
+        }, {
+            label: 'LowIncomeTracts',
+            type: 'num',
+            format: function (d) {
+                return d["LowIncomeTracts"];
+            }
+        }
+
         ])
-        .sortBy(function(d) {
+        .sortBy(function (d) {
             return d.Title;
         })
         .order(d3.ascending)
         .options({
             "scrollX": true
         })
-        .on('renderlet', function(table) {
+        .on('renderlet', function (table) {
             table.selectAll('.dc-table-group').classed('info', true);
         });
 
-    d3.selectAll('a#all').on('click', function() {
+    d3.selectAll('a#all').on('click', function () {
         dc.filterAll(groupname);
         dc.renderAll(groupname);
     });
 
 
-    d3.selectAll('a#county').on('click', function() {
+    d3.selectAll('a#county').on('click', function () {
         countyChart.filterAll(groupname);
         dc.redrawAll(groupname);
     });
 
-    d3.selectAll('a#lowIncome').on('click', function() {
+    d3.selectAll('a#lowIncome').on('click', function () {
         lowIncomeChart.filterAll(groupname);
         dc.redrawAll(groupname);
     });
 
-    $("#mapReset").on('click', function() {
+    $("#mapReset").on('click', function () {
         // paperMarkers.map().setView([24.5, 1.343465], 1);
         // paperMarkers.map().setView([47, -16.7], 1);
         paperMarkers.map().setView([19.7, 4.1], 2);
@@ -255,7 +386,7 @@ function draw(justic_data, food_data, nm_json) {
 
 
 
-$(document).ready(function() {
+$(document).ready(function () {
     $('[data-toggle="tooltip"]').tooltip();
 });
 
@@ -268,7 +399,7 @@ $(document).ready(function() {
 
 var navOffset = $('.navbar').height();
 
-$('.navbar li a').click(function(event) {
+$('.navbar li a').click(function (event) {
     var href = $(this).attr('href');
 
     event.preventDefault();
